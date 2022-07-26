99ba27b1a0bd16e68165affd9b5402cb2772350f
Yefim Blokh <73265655+yefim94@users.noreply.github.com>
2022-07-26 22:21:07 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@structs-and-linked-list-demo/main.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct sPerson {
+	int age;
+};
+
+struct sPerson *getNewPerson(const int age) 
+{
+    struct sPerson *newPerson = NULL;
+    newPerson = malloc(sizeof(struct sPerson));
+	newPerson->age = age;
+    printf("created new person at %p\n", newPerson);
+    return newPerson;
+}
+
+void printPerson(const struct sPerson *person, const char *comment)
+{
+    if (person == NULL)
+    {
+        pr, comment);
+    }
+    else
+    {
+       printf("%s: age:%d address:%p\n", 
+            comment,
+            person->age, 
+            person);
+    }
+    
+}
+
+int main() 
+{
+	printf("\n\n** START **\n\n");
+
+    
+    struct sPerson *first = NULL;
+    struct sPerson *second = NULL;
+
+    printPerson(first, "first");
+    printPerson(second, "second");
+    
+    first = getNewPerson(125);
+    second = getNewPerson(100);
+    
+    printPerson(first, "first");
+    printPerson(second, "second");
+
+    free(first);
+    free(second);    
+    
+    first = NULL;
+    second = NULL;
+
+	return 0;
+}
